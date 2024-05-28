6ddb333371bf04c4b929799927c38d6ec50c9bfc
Quang Anh <522562@student.fontys.nl>
2024-05-28 10:40:38 +0200

    Fixed HTML/CSS/JS layouting, Added some animations

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+// Variables
+let isMenuOn = false;
+const menu = document.querySelector(".menuButton");
+const menuLink = document.querySelector("#myLinks");
+
+// Sub functions
+function menuFunction() {
+  // If show then turn off
+  if (isMenuOn) {
+    isMenuOn = false;
+    menuLink.style.display = "none"
+  } else {
+    // Else off then turn on
+    isMenuOn = true;
+    menuLink.style.display = "block"
   }
+}
+
+// Main Function
+menu.addEventListener("click", menuFunction);
